7a82288dd26a1b94502d1d5afa5c2256a6bedbe8
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-07-22 11:58:46 +0530

    project structure

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+
 
 app.get('/', (req, res) => {
-    res.send('welcome')
+    res.render('home')
 })
+
+
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+
 
 app.listen(PORT, () => {
     console.log('Server started');
 })
-
-
