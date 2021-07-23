3c89551bdff2b9d7ab998d8b2bdb64ff837b8ade
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-07-23 17:10:58 +0530

    splitting up the routes with controllers

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const ejs = require('ejs');
-const path = require('path');
-const expressLayout = require('express-ejs-layouts');
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 
-
-
-
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('home')
-});
-
-app.get('/cart', (req, res) => {
-    res.render('customer/cart')
-})
-
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
-
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
+require("./routes/web")(app);
 
 app.listen(PORT, () => {
-    console.log('Server started');
-})
+  console.log("Server started");
+});
